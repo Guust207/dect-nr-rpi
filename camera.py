8f3881cf3345ad301624240719d904ab13241efe
Guust207 <92439850+Guust207@users.noreply.github.com>
2026-02-09 13:13:15 +0100

    Captured and stored 3 formats in camera

--- camera.py
+++ camera.py
@@ -1,14 +1,42 @@
-from picamera2 import Picamera2, Preview
+# Imports
 import time
+import os
+import io
 
+import cv2 as cv
+from picamera2 import Picamera2, Preview
+
+# Camera object init and preview start
 picam2 = Picamera2()
+picam2.options["quality"] = 45
+picam2.options["compress_level"] = 5
+capture_config = picam2.create_still_configuration()
+picam2.start(show_preview=True)
+time.sleep(1)
 
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
+# Capture image data
+# JPEG
+jpeg_data = io.BytesIO()
+picam2.capture_file(jpeg_data, format="jpeg")
+time.sleep(1)
 
-picam2.start_preview(Preview.QTGL)
-picam2.start()
+# PNG
+png_data = io.BytesIO()
+picam2.switch_mode_and_capture_file(capture_config, png_data, format="png")
+time.sleep(1)
 
-time.sleep(2)
+# BMP
+bmp_data = io.BytesIO()
+picam2.switch_mode_and_capture_file(capture_config, bmp_data, format="bmp")
+time.sleep(1)
 
-picam2.capture_file("test.jpg")
+# Print data size
+print(f"JPEG: {jpeg_data.getbuffer().nbytes} B")
+print(f"PNG: {png_data.getbuffer().nbytes} B")
+print(f"BMP: {bmp_data.getbuffer().nbytes} B")
+
+# Display the image
+#cv.imshow("Image", jpeg_data)
+#cv.waitKey(0)
+#cv.destroyAllWindows()
+

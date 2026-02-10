90f325457a3ea11f78ca678e1a9f591b706006bc
Guust207 <92439850+Guust207@users.noreply.github.com>
2026-02-10 15:08:04 +0100

    Alternative code added to camera

--- camera.py
+++ camera.py
@@ -10,7 +10,8 @@ import spidev
 
 # Camera object init and preview start
 picam2 = Picamera2()
-capture_config = picam2.create_still_configuration()
+picam2.options["quality"] = 90
+capture_config = picam2.create_still_configuration({"format": "YUV420"})
 picam2.start(show_preview=True)
 time.sleep(1)
 
@@ -19,9 +20,10 @@ img_data = io.BytesIO()
 picam2.capture_file(img_data, format="jpeg")
 
 # Print data size
-print(f"JPEG: {img_data.getbuffer().nbytes} B")
+tx_size = img_data.getbuffer().nbytes
 
 # Display the image
+picam2.stop_preview()
 img_arr = np.frombuffer(img_data.getbuffer().tobytes(), dtype=np.uint8)
 img = cv.imdecode(img_arr, cv.IMREAD_COLOR)
 cv.imshow("Image", img)
@@ -31,17 +33,24 @@ cv.destroyAllWindows()
 # SPI setup
 spi = spidev.SpiDev()
 tx_data = img_data.getbuffer().tobytes()
+print(f"Image data size (bytes): {tx_size} B")
+
 while True:
 	spi.open(0, 0)
 
 	spi.max_speed_hz = 1_000_000
-	spi.mode = 0
+	spi.mode = 3
 	spi.bits_per_word = 8
 
 	# SPI transfer
 	print(f"Tx data: {tx_data[0]}")
-	rx_data = spi.xfer3(tx_data)
-	print(f"Rx data: {rx_data[0]}")
+	#rx_data = spi.xfer3(tx_data, spi.max_speed_hz, 100)
+	CHUNK = 4096
+	for i in range(0, len(tx_data), CHUNK):
+		chunk = tx_data[i:i+CHUNK]
+		spi.xfer3(chunk)
+	
+	#print(f"Rx data: {rx_data[0]}")
 	
 	# Close and wait
 	spi.close()
